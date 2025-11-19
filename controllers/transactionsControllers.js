3a498fabcad7958e01b8a67156c9d326b61def20
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2025-11-20 00:28:20 +0530

    Changes in endpoint

--- controllers/transactionsControllers.js
+++ controllers/transactionsControllers.js
@@ -1,16 +1,24 @@
 import { pool } from "../db/db.js";
 
-export const getTransactionsDetails = async (req,res) => {
-    try{
+export const getTransactionsDetails = async (req, res) => {
+    try {
         const { account_no } = req.body;
-        
+
         if (!account_no) {
             return res.status(400).json({ error: "Account number is required" });
         }
 
-        const result = await pool.query("SELECT * FROM transactions WHERE account_no = $1",[account_no]);
-        res.json(result.rows)
-    } catch(err){
-        console.log("Error",err);
+        const result = await pool.query(
+            `SELECT * FROM transactions 
+             WHERE account_no = $1 
+                OR from_acc = $1 
+                OR to_acc = $1`,
+            [account_no]
+        );
+
+        res.json(result.rows);
+    } catch (err) {
+        console.log("Error", err);
+        res.status(500).json({ error: "Server error" });
     }
-}
+};
