5dfd0dad11c5b0c239631e2f6b5f050c8570a286
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2025-11-19 22:49:33 +0530

    Added new api

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,7 +8,7 @@ export const getUsers = async (req, res) => {
       return res.status(400).json({ error: "Email and phone are required" });
     }
 
-    const result = await pool.query("SELECT * FROM users WHERE email = $1 AND phone_no = $2",[email, phone]);
+    const result = await pool.query("SELECT phone_no, email, serial_no, name, upi_id, age, gender, language, address, pin_code FROM users WHERE email = $1 AND phone_no = $2",[email, phone]);
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -17,13 +17,13 @@ export const getUsers = async (req, res) => {
 
 export const getPassword = async (req, res) => {
   try {
-    const { account_no } = req.body;
+    const { email, phone } = req.body;
 
-    if (!account_no) {
-      return res.status(400).json({ error: "Account No. is required" });
+    if (!email || !phone) {
+      return res.status(400).json({ error: "Email and phone are required" });
     }
 
-    const result = await pool.query("SELECT password FROM users WHERE account_no = $1",[account_no]);
+    const result = await pool.query("SELECT password FROM users WHERE email = $1 AND phone_no = $2",[email, phone]);
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
