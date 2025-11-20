e0fa756b11eacf75e3868592a6449dc77c01cb8d
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2025-11-20 23:56:11 +0530

    Changes in endpoint

--- controllers/transactionsControllers.js
+++ controllers/transactionsControllers.js
@@ -9,12 +9,39 @@ export const getTransactionsDetails = async (req, res) => {
         }
 
         const result = await pool.query(
-            `SELECT * FROM transactions 
-             WHERE account_no = $1 
-                OR from_acc = $1 
-                OR to_acc = $1`,
+            `SELECT * FROM transactions
+                WHERE account_no = $1 
+                    OR from_acc = $1 
+                    OR to_acc = $1
+                ORDER BY created_at DESC`,
             [account_no]
         );
+
+
+        res.json(result.rows);
+    } catch (err) {
+        console.log("Error", err);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+export const getRecentTransactionDetails = async (req, res) => {
+    try {
+        const { account_no } = req.body;
+
+        if (!account_no) {
+            return res.status(400).json({ error: "Account number is required" });
+        }
+
+        const result = await pool.query(
+            `SELECT * FROM transactions
+                WHERE account_no = $1 
+                    OR from_acc = $1 
+                    OR to_acc = $1
+                ORDER BY created_at DESC LIMIT 1`,
+            [account_no]
+        );
+
 
         res.json(result.rows);
     } catch (err) {
--- routes/transactionsRoutes.js
+++ routes/transactionsRoutes.js
@@ -1,9 +1,10 @@
-import { getTransactionsDetails, transferMoney } from "../controllers/transactionsControllers.js";
+import { getTransactionsDetails, transferMoney, getRecentTransactionDetails } from "../controllers/transactionsControllers.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/getTransactionsDetails", getTransactionsDetails);
+router.post("/getRecentTransactionsDetails", getRecentTransactionDetails);
 router.post("/transferMoney", transferMoney);
 
 export default router;
